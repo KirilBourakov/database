4b748e6a18a7025f3555423c60372b23268a0f46
Kiril <kirbou06012@gmail.com>
2026-04-26 17:55:40 -0300

    added tests for slotted archetecture

--- tests/all_tests.c
+++ tests/all_tests.c
@@ -1,40 +1,35 @@
 #include "../lib/unity/unity.h"
-#include "../src/table.h"
-#include "../src/page.h"
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 
-void setUp(void) {
-    remove("test.data");
-}
-
-void tearDown(void) {
-    remove("test.data");
-}
+void setUp(void) {}
+void tearDown(void) {}
 
 // Table tests
-void test_SingleRowWriteRead(void);
-void test_MultipleRowsAppend(void);
+void test_malloc_dealloc_row(void);
+void test_pack_unpack_row(void);
 
 // Page tests
 void test_create_page(void);
 void test_destroy_page(void);
-void test_page_insert_success(void);
-void test_page_insert_full(void);
+void test_page_insert(void);
+void test_slot_data(void);
+void test_page_full(void);
 
 int main(void) {
     UNITY_BEGIN();
     
     // Table Tests
-    RUN_TEST(test_SingleRowWriteRead);
-    RUN_TEST(test_MultipleRowsAppend);
+    RUN_TEST(test_malloc_dealloc_row);
+    RUN_TEST(test_pack_unpack_row);
     
     // Page Tests
     RUN_TEST(test_create_page);
     RUN_TEST(test_destroy_page);
-    RUN_TEST(test_page_insert_success);
-    RUN_TEST(test_page_insert_full);
+    RUN_TEST(test_page_insert);
+    RUN_TEST(test_slot_data);
+    RUN_TEST(test_page_full);
     
     return UNITY_END();
 }
--- tests/test_page.c
+++ tests/test_page.c
@@ -14,7 +14,7 @@ void test_create_page(void) {
     TEST_ASSERT_EQUAL_UINT16(sizeof(PageHeader), header->slot_array_end);
     TEST_ASSERT_EQUAL_UINT16(PAGE_SIZE, header->row_data_start);
 
-    free(page);
+    destroy_page(&page);
 }
 
 void test_destroy_page(void) {
@@ -23,58 +23,51 @@ void test_destroy_page(void) {
     TEST_ASSERT_NULL(page);
 }
 
-void test_page_insert_success(void) {
+void test_page_insert(void) {
     DbPage* page = create_page(1);
-    char* row1 = "Hello";
-    uint16_t size1 = strlen(row1) + 1;
-    
-    int id1 = page_insert(page, row1, size1);
-    TEST_ASSERT_EQUAL_INT(0, id1);
+    char* row = "Test Data";
+    uint16_t size = strlen(row) + 1;
+
+    int slot = page_insert(page, row, size);
+    TEST_ASSERT_EQUAL_INT(0, slot);
 
     PageHeader* header = (PageHeader*)page->data;
     TEST_ASSERT_EQUAL_UINT16(1, header->num_slots);
     TEST_ASSERT_EQUAL_UINT16(sizeof(PageHeader) + PAGE_SLOT_SIZE, header->slot_array_end);
-    TEST_ASSERT_EQUAL_UINT16(PAGE_SIZE - size1, header->row_data_start);
-    TEST_ASSERT_EQUAL_MEMORY(row1, page->data + header->row_data_start, size1);
-
-    char* row2 = "World!!!";
-    uint16_t size2 = strlen(row2) + 1;
-    int id2 = page_insert(page, row2, size2);
-    TEST_ASSERT_EQUAL_INT(1, id2);
-
-    TEST_ASSERT_EQUAL_UINT16(2, header->num_slots);
-    TEST_ASSERT_EQUAL_UINT16(sizeof(PageHeader) + 2 * PAGE_SLOT_SIZE, header->slot_array_end);
-    TEST_ASSERT_EQUAL_UINT16(PAGE_SIZE - size1 - size2, header->row_data_start);
-    TEST_ASSERT_EQUAL_MEMORY(row2, page->data + header->row_data_start, size2);
-
-    // Verify slots
-    uint16_t offset1;
-    memcpy(&offset1, page->data + sizeof(PageHeader), PAGE_SLOT_SIZE);
-    TEST_ASSERT_EQUAL_UINT16(PAGE_SIZE - size1, offset1);
-
-    uint16_t offset2;
-    memcpy(&offset2, page->data + sizeof(PageHeader) + PAGE_SLOT_SIZE, PAGE_SLOT_SIZE);
-    TEST_ASSERT_EQUAL_UINT16(PAGE_SIZE - size1 - size2, offset2);
+    TEST_ASSERT_EQUAL_UINT16(PAGE_SIZE - size, header->row_data_start);
 
     destroy_page(&page);
 }
 
-void test_page_insert_full(void) {
+void test_slot_data(void) {
     DbPage* page = create_page(1);
-    PageHeader* header = (PageHeader*)page->data;
+    char* row = "Slot Test";
+    uint16_t size = strlen(row) + 1;
 
-    // Fill the page almost completely
-    uint16_t large_row_size = PAGE_SIZE - sizeof(PageHeader) - PAGE_SLOT_SIZE;
-    void* large_row = malloc(large_row_size);
-    memset(large_row, 'A', large_row_size);
+    int slot = page_insert(page, row, size);
+    void* data = slot_data(page, slot);
 
-    int id = page_insert(page, large_row, large_row_size);
-    TEST_ASSERT_EQUAL_INT(0, id);
+    TEST_ASSERT_NOT_NULL(data);
+    TEST_ASSERT_EQUAL_STRING(row, (char*)data);
 
-    // Try to insert one more byte, should fail because we need space for slot too
-    char small_row = 'B';
-    int fail_id = page_insert(page, &small_row, 1);
-    TEST_ASSERT_EQUAL_INT(-1, fail_id);
+    destroy_page(&page);
+}
+
+void test_page_full(void) {
+    DbPage* page = create_page(1);
+
+    // Max data we can fit is PAGE_SIZE - header - 1 slot
+    uint16_t max_size = PAGE_SIZE - sizeof(PageHeader) - PAGE_SLOT_SIZE;
+    void* large_row = malloc(max_size);
+    memset(large_row, 'A', max_size);
+
+    int slot = page_insert(page, large_row, max_size);
+    TEST_ASSERT_EQUAL_INT(0, slot);
+
+    // This should fail
+    char* small_row = "Fail";
+    int fail_slot = page_insert(page, small_row, 5);
+    TEST_ASSERT_EQUAL_INT(-1, fail_slot);
 
     free(large_row);
     destroy_page(&page);
